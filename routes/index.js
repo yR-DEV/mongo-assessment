027edc40f8c8686cef5de19e2937a56b019bb9ff
Zack Claar <claar.zack@gmail.com>
2015-08-17 11:41:10 -0600

    built out validation a little, installed cookkie session and bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('monk')('localhost/textWriter');
+var accountValidation = require('../lib/javascripts/accountValidation.js');
+var mongoCalls = require('../lib/javascripts/mongoCalls.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,8 +14,14 @@ router.get('/accountLogin', function(req, res, next) {
   res.render('accountLogin');
 });
 
+//getting acount creation page
 router.get('/accountCreate', function(req, res, next) {
   res.render('accountCreate');
+});
+
+//getting account information page
+router.get('/account', function(req, res, next) {
+  res.render('account');
 });
 
 //posting on index
@@ -25,7 +33,23 @@ router.post('/', function(req, res, next) {
   } else if (command === 'create') {
     res.redirect('/accountCreate');
   }
-})
+});
+
+//posting when creating an accountLogin
+router.post('/accountCreate', function(req, res, next) {
+  var username = req.body.textWriterNewUsername;
+  var pass1 = req.body.textWriterNewPassword;
+  var pass2 = req.body.textWriterNewPassword2;
+  var userCheck = accountValidation.usernameCheck(username);
+  var errors = accountValidation.newAccountValidation(username, pass1, pass2);
+  console.log(userCheck);
+  console.log(errors);
+  if(errors.length > 0) {
+    res.render('account',{errors: errors});
+  } else {
+    res.redirect('/account');
+  }
+});
 
 
 module.exports = router;
