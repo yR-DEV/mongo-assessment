149d10e52a42808264235806c61d6f45133faffe
Zack Claar <claar.zack@gmail.com>
2015-08-17 10:18:47 -0600

    Index up, if user enters create, redirected to create account page, if they enter login, redirect to login page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/textWriter');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//getting login page
+router.get('/accountLogin', function(req, res, next) {
+  res.render('accountLogin');
+});
+
+router.get('/accountCreate', function(req, res, next) {
+  res.render('accountCreate');
+});
+
+//posting on index
+router.post('/', function(req, res, next) {
+  var command = req.body.textWriterIndexCommand;
+  command.toLowerCase().trim();
+  if(command === 'login') {
+    res.redirect('/accountLogin');
+  } else if (command === 'create') {
+    res.redirect('/accountCreate');
+  }
+})
+
 
 module.exports = router;
